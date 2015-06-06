59c9c185db9bdd46fbb69a393b82d2bdbde69c79
Maggie Leake <magg8474@rackspace.com>
2015-06-06 09:15:51 -0500

    moved Map to it's own file.

---
+++ Map.py
@@ -0,0 +1,22 @@
+from Room import EndRoom
+
+class Map(object):
+    def __init__(self, *args):
+        if len(args) > 0:
+            self.path = args
+            self.current_room = -1
+        else:
+            return EndRoom()
+
+    def next_scene(self):
+        self.current_room = self.current_room+1
+        if self.current_room >= len(self.path):
+            return EndRoom()
+        else:
+            return self.path[self.current_room]
+        
+    def move_back_to_start(self):
+        self.current_room = -1
+        
+    def move_to_end(self):
+        self.current_room = len(self.path)
--- Trapped.py
+++ Trapped.py
@@ -1,71 +1,6 @@
-from sys import exit
-from Room import FirstRoom, EndRoom
+from Room import FirstRoom
+from Map import Map
 
-class Scene(object):
-
-    def enter(self):
-        print "Not ready yet"
-        exit(1)
-
-
-class Engine(object):
-
-    def __init__(self, scene_map):
-        self.scene_map = scene_map
-
-    def play(self):
-        current_scene = self.scene_map.opening_scene()
-        last_scene = self.scene_map.next_scene('finished')
-
-        while current_scene != last_scene:
-            next_scene_name = current_scene.enter()
-            current_scene = self.scene_map.next_scene(next_scene_name)
-
-        # be sure to print out the last scene
-        current_scene.enter()
-        
-
-class Death(Scene):
-
-    def enter(self):
-        pass
-
-class Rock(Scene):
-
-    def enter(self):
-        pass
-
-class Party(Scene):
-
-    def enter(self):
-        pass
-
-class Snakes(Scene):
-
-    def enter(self):
-        pass
-
-
-class Map(object):
-    def __init__(self, *args):
-        if len(args) > 0:
-            self.path = args
-            self.current_room = -1
-        else:
-            return EndRoom()
-
-    def next_scene(self):
-        self.current_room = self.current_room+1
-        if self.current_room >= len(self.path):
-            return EndRoom()
-        else:
-            return self.path[self.current_room]
-        
-    def move_back_to_start(self):
-        self.current_room = -1
-        
-    def move_to_end(self):
-        self.current_room = len(self.path)
 
 #Start the game
 a_map = Map(FirstRoom())
@@ -80,7 +15,3 @@ while not current_room.end_room():
         current_room.procces_commands(string_from_input)
     current_room = a_map.next_scene()
 current_room.enter_room()
-
-#room = a_map.next_scene()
-#a_game = Engine(a_map)
-#a_game.play()
