40f07c5a3a5ef42e4c08160a1d5f51b421317664
Erica Forget <erica.forget@gmail.com>
2015-06-06 13:43:23 -0500

    code issue

--- Room.py
+++ Room.py
@@ -1,4 +1,4 @@
-from Command import Commands, Command, Panic, Climb, Sit
+from Command import Commands, Command, Panic, Climb, Sit, Take
 from Room_Object import RoomObjects, RoomObject, KeyRoomOne, Chair, VLrock
 
 
@@ -31,7 +31,7 @@ class Room(object):
 class FirstRoom(Room):
 
     def __init__(self,inventory):
-        room_objects = RoomObjects(KeyRoomOne(),VLrock(),RoomObject("door","a thick metal panel with a handle"),Chair())
+        room_objects = RoomObjects(KeyRoomOne(),VLrock(),Chair())
         commands = Commands(Command("take"),Panic(), Sit(), Climb(inventory,room_objects))
         super(FirstRoom, self).__init__(commands,room_objects,inventory)
         self.done = False
